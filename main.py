05bb3f215a91ba22078ebe4d2f8d4a0e2ff80490
Aditya Vaish <adityavaish846@gmail.com>
2025-07-27 13:58:23 +0530

    init commit

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-#API-Tower
+#API Tower
+from fastapi import FastAPI
 
-import fastapi as FastAPI
 app = FastAPI()
+
+@app.get("/")
+def get_data():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return {"item_id": item_id}
+
+
+
